716c63d529d5710764d5d14c1d7bab5bbda8680f
Jeong Sohyun <84312457+kokiok3@users.noreply.github.com>
2025-12-28 00:58:45 +0900

    Feat: 동적 메타데이터 추가

--- app/(home)/movie/[id]/page.tsx
+++ app/(home)/movie/[id]/page.tsx
@@ -1,40 +1,55 @@
 import { Metadata } from "next";
 import { API_URL } from "../../page";
-import MovieInfo from "../../../../component/movie-info";
+import MovieInfo, { getMovie } from "../../../../component/movie-info";
 import MovieVideos from "../../../../component/movie-videos";
 import { Suspense } from "react";
 
-export const metadata: Metadata = {
-    title: 'Movie Detail',
+// export const metadata: Metadata = {
+//     title: 'Movie Detail',
+// }
+
+interface IParams {
+    params: Promise<{ id: string }>;
+    searchParams: Promise<{ name: string }>;
 }
 
-async function getMovie(id: string){
-    console.log('fetching movies: ', Date.now());
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}`);
-    return response.json();
-}
-async function getTrailer(id: string){
-    console.log('fetching trailer: ', Date.now());
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    const response = await fetch(`${API_URL}/${id}/videos`);
-    return response.json();
+
+export async function generateMetadata({ params }: IParams) {
+    const id = (await params).id;
+    const movie = await getMovie(id);
+    return {
+        title: movie.title
+    };
 }
 
-export default async function MovieDetailPage({params, searchParams}: {params: Promise<{id: string}>, searchParams: Promise<{name: string}>}) {
-    const {id} = await params;
-    const {name} = await searchParams;
+// async function getMovie(id: string) {
+//     console.log('fetching movies: ', Date.now());
+//     await new Promise(resolve => setTimeout(resolve, 5000));
+//     const response = await fetch(`${API_URL}/${id}`);
+//     return response.json();
+// }
+// async function getTrailer(id: string) {
+//     console.log('fetching trailer: ', Date.now());
+//     await new Promise(resolve => setTimeout(resolve, 5000));
+//     const response = await fetch(`${API_URL}/${id}/videos`);
+//     return response.json();
+// }
+
+export default async function MovieDetailPage({ params, searchParams }: IParams) {
+    const { id } = await params;
+    const { name } = await searchParams;
 
     // 직렬 처리
     // const movie = await getMovie(id);
     // const trailer = await getTrailer(id);
-    
+
     // 병렬 처리
     // const [movie, trailer] = await Promise.all([getMovie(id), getTrailer(id)]);
     return (
         <div>
-            <h1>movie detail : id: {id}, name: {name}</h1>
-            
+            {/* <h1>movie detail : id: {id}, name: {name}</h1> */}
+
+
             {/* 병렬처리 */}
             {/* <div>{movie.title}</div>
             <div>{trailer.length}</div> */}
